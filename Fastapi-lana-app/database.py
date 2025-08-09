876f2fb2f020d8c9aa627bfd095fbc156c135181
Kripxx <125685085+Kripxx@users.noreply.github.com>
2025-08-08 19:57:50 -0600

    mio

--- Fastapi-lana-app/database.py
+++ Fastapi-lana-app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # ⚠️ Reemplaza 'root', 'tu_contraseña' y 'localhost' según tu configuración:
-DATABASE_URL = "mysql+pymysql://root:12358@localhost:3306/lana_app?charset=utf8mb4"
+DATABASE_URL = "mysql+pymysql://root:12358@localhost:3306/lana_app"
 
 
 engine = create_engine(DATABASE_URL)
